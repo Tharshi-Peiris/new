b80eb68e9aa4f88aed9988755a6ccd13cc6921d7
Tharushi Peiris <122695047+Tharshi-Peiris@users.noreply.github.com>
2026-07-06 14:11:59 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCabOf-rYYZ2eV6Bu1BApKyqYyd8nHSYYQ",
+  authDomain: "sylvaracom.firebaseapp.com",
+  projectId: "sylvaracom",
+  storageBucket: "sylvaracom.firebasestorage.app",
+  messagingSenderId: "818423713268",
+  appId: "1:818423713268:web:fd097497cd745704413da1",
+  measurementId: "G-E4P55R64GL"
+};
